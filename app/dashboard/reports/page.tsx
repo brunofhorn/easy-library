6c24e41b9ff75e76e51b026cc567db17c019e796
brunofhorn <b.fernandeshorn@gmail.com>
2023-11-24 17:33:55 -0300

    feat: add reports page

---
+++ app/dashboard/reports/page.tsx
@@ -0,0 +1,11 @@
+import TitlePage from "@/components/layout/title-page";
+import Typography from "@/components/shared/typography";
+
+export default async function ReportsPage() {
+    return (
+        <div>
+            <TitlePage title={"Relatórios"} />
+            <Typography variant="h6">Aguarde! Em construção.</Typography>
+        </div>
+    );
+}
