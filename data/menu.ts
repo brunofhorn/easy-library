5834724313b89517efcf4974cf4578842958c470
brunofhorn <b.fernandeshorn@gmail.com>
2023-11-24 03:21:03 -0300

    feat: change menu href

--- data/menu.ts
+++ data/menu.ts
@@ -6,6 +6,6 @@ export const menus: IMenu[] = [
     { id: 3, name: "Autores", icon: "uiw:user-add", active: false, color: "", href: "/dashboard/author" },
     { id: 4, name: "Editoras", icon: "grommet-icons:book", active: false, color: "", href: "/dashboard/publishingCompany" },
     { id: 5, name: "Leitores", icon: "uil:book-reader", active: false, color: "", href: "/dashboard/reader" },
-    { id: 6, name: "Empréstimos", icon: "carbon:global-loan-and-trial", active: false, color: "/dashboard/loans" },
-    { id: 7, name: "Relatórios", icon: "tabler:report", active: false, color: "/dashboard/reports" },
+    { id: 6, name: "Empréstimos", icon: "carbon:global-loan-and-trial", active: false, color: "", href: "/dashboard/loans" },
+    { id: 7, name: "Relatórios", icon: "tabler:report", active: false, color: "", href: "/dashboard/reports" },
 ];
